08c4695dec64d176d1a429d2310a04f32b050fc9
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-11-15 17:14:41 +0000

    finished product

--- interface.js
+++ interface.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var thermostat = new Thermostat();
 
   function updateTemperature() {
@@ -40,4 +41,12 @@ $(document).ready(function() {
       $('.PSM').text('Power Saving Mode: ON');
     }
   });
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp + '°C');
+    })
+  })
 })
